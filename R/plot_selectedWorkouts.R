73d004e1e69c34d8d7c8fdc0d061cbf7b97605b7
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2019-02-13 18:28:20 +0000

    Added vertical lines for changepoints

--- R/plot_selectedWorkouts.R
+++ R/plot_selectedWorkouts.R
@@ -157,11 +157,16 @@ plot_selectedWorkouts <- function(x, session, what, sumX, threshold = TRUE, smoo
         # initiate a line shape object
         line <- list(
           type = "line",
-          line = list(color = "darkred", dash = "dash"),
+          line = list(color = "darkred", dash = "dot"),
           xref = paste0("x", n_plot),
           yref = paste0("y", n_plot)
         )
-
+        line_v <- list(
+          type = "line",
+          line = list(color = "darkblue", dash = "dot"),
+          xref = paste0("x", n_plot),
+          yref = paste0("y", n_plot)
+        )
 
         for (k in c(1:(length(x_values) - 1))) {
           line[["x0"]] <- df_subset$Index[x_values[k]]
@@ -170,7 +175,17 @@ plot_selectedWorkouts <- function(x, session, what, sumX, threshold = TRUE, smoo
           changepoint_y_values <- c(changepoint_y_values, y_values[k])
           shapes[[length(shapes) + 1]] <- line
         }
+        for (k in c(1:(length(x_values) - 1))) {
+          line_v[["x0"]] <- df_subset$Index[x_values[k]]
+          line_v[["x1"]] <- df_subset$Index[x_values[k]]
+          line_v[c("y0", "y1")] <- range(df_subset$Value)
+          changepoint_y_values <- c(changepoint_y_values, y_values[k])
+          shapes[[length(shapes) + 1]] <- line_v
+        }
+
+
       }
+
       sampled_rows <- sort(sample(zoo::index(df_subset),
                                   size = length(zoo::index(df_subset)) * desampling))
       a <- plotly::plot_ly(
